75bb655dcee488dd5a029f8d91444099196f247f
TerabyteTB <raymondan0831@gmail.com>
2022-12-04 20:32:42 -0800

    The basics

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
+import math.Functions;
+
 public class Main {
     public static void main(String[] args) {
+        int n = 5;
 
-        System.out.println("Hello world!");
-
+        System.out.println(Functions.GAUSS_ADDITION(n));
     }
-
 }
---
+++ src/math/Functions.java
@@ -0,0 +1,10 @@
+package math;
+
+public class Functions {
+
+    public static int GAUSS_ADDITION (int n) {
+        n = n * (n + 1) / 2;
+
+        return n;
+    }
+}

7f19030795ca48154cc7657f074d5c698fda13ad
TerabyteTB <raymondan0831@gmail.com>
2023-09-01 19:14:42 -0700

    Random thing

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ import math.Functions;
 public class Main {
     public static void main(String[] args) {
         int n = 5;
+        System.out.println(Functions.gaussAddition(n));
 
-        System.out.println(Functions.GAUSS_ADDITION(n));
+        System.out.println(Functions.dateToDay(1999, 11, 21));
     }
 }
--- src/math/Functions.java
+++ src/math/Functions.java
@@ -1,10 +1,27 @@
 package math;
 
 public class Functions {
-
-    public static int GAUSS_ADDITION (int n) {
+    public static int gaussAddition(int n) {
         n = n * (n + 1) / 2;
 
         return n;
+    }
+
+    public static String dateToDay(int year, int month, int day){
+        double f = (14.00 - month) / 12;
+        double y = year - f;
+        double m = month + 12 * f - 2;
+
+        int d = (int) ((day + y + (31 * m / 12) + (y / 4) - (y / 100) + (y / 400)) % 7);
+        return switch (d) {
+            case 0 -> "Monday";
+            case 1 -> "Tuesday";
+            case 2 -> "Wednesday";
+            case 3 -> "Thursday";
+            case 4 -> "Friday";
+            case 5 -> "Saturday";
+            case 6 -> "Sunday";
+            default -> throw new IllegalStateException("Unexpected value: " + d);
+        };
     }
 }

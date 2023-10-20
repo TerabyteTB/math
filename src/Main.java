5b88830092962a1bf1b9477ea2d2ab8cb2b158ab
TerabyteTB <raymondan0831@gmail.com>
2023-10-20 10:34:17 -0700

    Added comments

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,8 @@ import java.util.stream.IntStream;
 public class Main {
 
     public static void main(String[] args) {
-        int n = 100;
-        int[] numbers = IntStream.iterate(1, i -> i <= n, i -> i + 1)
+        int n = 100; //limiter
+        int[] numbers = IntStream.iterate(1, i -> i <= n, i -> i + 1) //adds all numbers from 1 to n to array
                 .boxed()
                 .toList().stream()
                 .mapToInt(Integer::intValue).toArray();
@@ -21,9 +21,10 @@ public class Main {
 
         Arrays.sort(numbers);
 
-        for (int i = 0; i < numbers.length - 2; i++) {
-            for (int j = i + 1; j < numbers.length - 1; j++) {
-                for (int k = j + 1; k < numbers.length; k++) {
+        //loops to avoid duplicate numbers
+        for (int i = 0; i < numbers.length - 2; i++) { //set 1
+            for (int j = i + 1; j < numbers.length - 1; j++) { //set 2
+                for (int k = j + 1; k < numbers.length; k++) { //set 3
                     int a = numbers[i];
                     int b = numbers[j];
                     int c = numbers[k];
@@ -40,20 +41,20 @@ public class Main {
     }
 
     public static List<List<Integer>> equalPnATriangles(List<List<Integer>> triangles) {
-        List<List<Integer>> result = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>(); //List that the code will add triangles to
 
-        for (List<Integer> triangle : triangles) {
+        for (List<Integer> triangle : triangles) { //iterate through List with each of the terms being a list with 3 values
             if (triangle.size() == 3) {
                 int a = triangle.get(0);
                 int b = triangle.get(1);
                 int c = triangle.get(2);
 
                 if (isTriangle(a, b, c)) {
-                    double perimeter = a + b + c;
-                    double s = perimeter / 2.0;
+                    double p = a + b + c;
+                    double s = p / 2.0; //semi-perimeter!!11!!1!1
                     double area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
 
-                    if (perimeter == area) {
+                    if (p == area) { //if perimeter equals area, add said List to the result
                         result.add(triangle);
                     }
                 }
@@ -63,7 +64,7 @@ public class Main {
         return result;
     }
 
-    public static boolean isTriangle(int a, int b, int c) {
+    public static boolean isTriangle(int a, int b, int c) { //verify if triangle
         return (a + b > c) && (a + c > b) && (b + c > a);
     }
 }
